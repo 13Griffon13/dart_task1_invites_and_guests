d2e86d4ac644ab58ffdbf468cd857d58b8a42140
Hrihoriy <hryhoriy.kysil@chisw.com>
2022-11-18 18:17:51 +0200

    added task2(futures)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:task1_invites/calculator.dart';
 
@@ -24,17 +25,37 @@ void main() {
 
   fromString(stringInput);
   fromJson(jsonInput);
+  fromFile("lib/mockup_data/response.json");
 }
 
 //task1
-void fromString(String string){
+void fromString(String string) {
   var parsedResponse = GuestResponses.fromFormattedString(string);
   print('String total sum is: ${Calculator.calculate(parsedResponse)}');
 }
 
 //task3
-void fromJson(String jsonInput){
+void fromJson(String jsonInput) {
   var decodedJson = jsonDecode(jsonInput);
   var parsedResponse = GuestResponses.fromJson(decodedJson);
   print('JSON total sum is: ${Calculator.calculate(parsedResponse)}');
+}
+
+//task2
+void fromFile(String path) async {
+  final responseFileRef = File(path);
+  final responseContent = await responseFileRef.readAsString();
+  final decodedResponse = jsonDecode(responseContent);
+  final listOfGuestSource = decodedResponse["guests"] as List<dynamic>;
+  var inviteRequests = <Future<String>>[];
+  for (var element in listOfGuestSource) {
+    inviteRequests.add(File(element as String).readAsString());
+  }
+  final guests = await Future.wait(inviteRequests).then((value) {
+    return value.map((e) => Guest.fromJson(jsonDecode(e))).toList();
+  });
+  final parsedResponse = GuestResponses(
+      invites: guests,
+      numberOfGuests: decodedResponse["numberOfGuests"] as int);
+  print('Async total sum is: ${Calculator.calculate(parsedResponse)}');
 }
